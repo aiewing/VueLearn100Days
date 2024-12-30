6aed8f108068f97fdd33c1b0d35e4c19585fc02c
Aiewing <pfm1197@dingtalk.com>
2024-12-30 11:39:52 +0800

    09 | 动画：Vue 中如何实现动画效果

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,14 @@ import ColorChange from './components/ColorChange.vue';
 import Favicon from './components/Favicon.vue';
 import FullScreen from './components/FullScreen.vue';
 import ShowRate from './components/8-Components/ShowRate.vue';
+import WidthChange from './components/9-Animation/WidthChange.vue';
+import ShowToggle from './components/9-Animation/ShowToggle.vue';
+import TodoListUpdate from './components/9-Animation/TodoListUpdate.vue';
 
 </script>
 
 <template>
-  <ShowRate />
+  <TodoListUpdate />
 </template>
 
 <style scoped>
---
+++ src/components/9-Animation/ShowToggle.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <button @click="toggle">Click</button>
+    <transition name="fade">
+        <h1 v-if="showToggle">Hello Vue 3</h1>
+    </transition>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+let showToggle = ref(true)
+function toggle() {
+    showToggle.value = !showToggle.value
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s linear;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
